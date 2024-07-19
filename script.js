93d734a1ed7687663729f9cb3ececb9dc6419d89
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-07-19 17:45:07 +0000

    Add number selector

--- script.js
+++ script.js
@@ -20,6 +20,61 @@ let secondNumber;
 
 let operator;
 
+let displayValue = "";
+
+let currentFocusOnStart = true;
+
 const operate = (operator, num1, num2) => {
   return operator(num1, num2);
 };
+
+let numbers = document.querySelectorAll(".number-button");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    let currentNumber = event.target.id;
+
+    if (currentFocusOnStart === true) {
+      if (firstNumber === undefined) {
+        firstNumber = currentNumber;
+      } else {
+        firstNumber = firstNumber + currentNumber;
+        // displayValue = firstNumber.toString();
+      }
+      console.log(firstNumber);
+    } else if (currentFocusOnStart === false) {
+      if (secondNumber === undefined) {
+        secondNumber = currentNumber;
+      } else {
+        secondNumber = secondNumber + currentNumber;
+      }
+      console.log(secondNumber);
+    }
+  });
+});
+
+// number.addEventListener("click", (event) => {
+//   let currentNumber = event.target.id;
+//   if (currentFocusOnStart === true) {
+//     if (firstNumber === undefined) {
+//       firstNumber = currentNumber;
+//     } else {
+//       firstNumber = firstNumber + currentNumber;
+//       // displayValue = firstNumber.toString();
+//     }
+//     console.log(firstNumber);
+//   } else if (currentFocusOnStart === false) {
+//     if (secondNumber === undefined) {
+//       secondNumber = currentNumber;
+//     } else {
+//       secondNumber = secondNumber + currentNumber;
+//     }
+//     console.log(secondNumber);
+//   }
+// });
+
+let operators = document.querySelector(".operator-button");
+
+operators.addEventListener("click", () => {
+  currentFocusOnStart = false;
+});
