0c1256e51f278d682ef4419c22b5adf1ced39675
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-07-19 18:01:22 +0000

    Basic calculation works

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let displayValue = "";
 let currentFocusOnStart = true;
 
 const operate = (operator, num1, num2) => {
-  return operator(num1, num2);
+  return operator(+num1, +num2);
 };
 
 let numbers = document.querySelectorAll(".number-button");
@@ -53,28 +53,38 @@ numbers.forEach((number) => {
   });
 });
 
-// number.addEventListener("click", (event) => {
-//   let currentNumber = event.target.id;
-//   if (currentFocusOnStart === true) {
-//     if (firstNumber === undefined) {
-//       firstNumber = currentNumber;
-//     } else {
-//       firstNumber = firstNumber + currentNumber;
-//       // displayValue = firstNumber.toString();
-//     }
-//     console.log(firstNumber);
-//   } else if (currentFocusOnStart === false) {
-//     if (secondNumber === undefined) {
-//       secondNumber = currentNumber;
-//     } else {
-//       secondNumber = secondNumber + currentNumber;
-//     }
-//     console.log(secondNumber);
-//   }
-// });
+let addButton = document.querySelector("#add");
 
-let operators = document.querySelector(".operator-button");
-
-operators.addEventListener("click", () => {
+addButton.addEventListener("click", () => {
+  operator = add;
   currentFocusOnStart = false;
+});
+
+let subtractButton = document.querySelector("#subtract");
+
+subtractButton.addEventListener("click", () => {
+  operator = subtract;
+  currentFocusOnStart = false;
+});
+
+let multiplyButton = document.querySelector("#multiply");
+
+multiplyButton.addEventListener("click", () => {
+  operator = multiply;
+  currentFocusOnStart = false;
+});
+
+let divideButton = document.querySelector("#divide");
+
+divideButton.addEventListener("click", () => {
+  operator = divide;
+  currentFocusOnStart = false;
+});
+
+let resultButton = document.querySelector("#result");
+
+resultButton.addEventListener("click", () => {
+  console.log(firstNumber);
+  console.log(secondNumber);
+  console.log(operate(operator, firstNumber, secondNumber));
 });
